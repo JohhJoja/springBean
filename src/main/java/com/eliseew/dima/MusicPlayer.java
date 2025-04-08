0b0061b250df7c52e6df8397b5b60c5030d26b71
JohhJoja <08022002dimaeliseew@gmail.com>
2025-04-08 15:57:16 +0300

    ref+setter+prop.placeholder

--- src/main/java/com/eliseew/dima/MusicPlayer.java
+++ src/main/java/com/eliseew/dima/MusicPlayer.java
@@ -2,11 +2,25 @@ package com.eliseew.dima;
 
 public class MusicPlayer {
     private Music music;
+    private String volume;
     //IoC
     public MusicPlayer(Music music) {
         this.music = music;
     }
-    public void playMusic(){
-        System.out.println("Playing: "+music.getSong());
+
+    public void setVolume(String volume) {
+        this.volume = volume;
     }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public MusicPlayer() {}
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void playMusic(){System.out.println("Playing: "+music.getSong());}
 }
